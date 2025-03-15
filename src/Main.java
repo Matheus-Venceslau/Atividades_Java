b9aebb4781b6db81c68b65a87710a8985dd56d39
Matheus Venceslau <matheushlvenceslau@gmail.com>
2025-03-15 12:10:02 -0300

    feat: Conceitos importantes em Java

--- src/Main.java
+++ src/Main.java
@@ -14,9 +14,52 @@ public class Main {
         System.out.println("A média do inteiro é: " + media01);
         System.out.println("A média do real é: " + media);
 
+        System.out.println();
+
         // Declaração de Long
         long distancia = 149600000L;
 
         System.out.println("A distância da Terra ao Sol é: " + distancia + " km.");
+
+        System.out.println();
+
+        // Valor Unicode de um caractere
+
+        char letra1 = 'A';
+        char letra2 = 'X';
+        char letra3 = 'm';
+        char letra4 = 'z';
+        char letra5 = 'K';
+        char letra6 = 't';
+
+        System.out.println("O valor Unicode de '" + letra1 + "' é: " + (int) letra1);
+        System.out.println("O valor Unicode de '" + letra2 + "' é: " + (int) letra2);
+        System.out.println("O valor Unicode de '" + letra3 + "' é: " + (int) letra3);
+        System.out.println("O valor Unicode de '" + letra4 + "' é: " + (int) letra4);
+        System.out.println("O valor Unicode de '" + letra5 + "' é: " + (int) letra5);
+        System.out.println("O valor Unicode de '" + letra6 + "' é: " + (int) letra6);
+
+      // Usando arrays
+
+        int[] numeros = {17770, 20, 30, 474545112, 50, 70, 68, 99};
+        System.out.println("O quarto valor é: " + numeros[3]);
+
+        // Exercício 05 - Área do retângulo
+
+        double largura = 5.0;
+        double altura = 3.0;
+
+        double area = largura * altura;
+
+        System.out.println("A area do retângulo é: " + area);
+
+        // Trabalhando com strings e dados multivalorados
+
+        String nome = "Matheus";
+        int idade = 46;
+        double peso = 72.7;
+
+        System.out.println("Olá meu nome é: " + nome + "Minha idade é: " + idade + "Meu peso é: " + peso);
+
     }
 }
