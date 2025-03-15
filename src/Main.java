5b4918bc34af0de18aa28f18fab7ea9bdc37204d
Matheus Venceslau <matheushlvenceslau@gmail.com>
2025-03-15 10:42:44 -0300

    feat: Atividade Java

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        // Média de três números
+        int num01 = 77;
+        int num02 = 71;
+        int num03 = 77;
+
+        double media = (num01 + num02 + num03) / 3.0;
+        int media01 = (num01 + num02 + num03) / 3;
+
+        System.out.println("A média do inteiro é: " + media01);
+        System.out.println("A média do real é: " + media);
+
+    }
+}
